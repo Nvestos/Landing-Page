715f5674af327c088fb618578d2977e2d04c8678
Ogurinka Tammy Benjamin <ogurinkabenjamin@gmail.com>
2023-05-08 12:50:38 +0100

    update

---
+++ src/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  document.body.classList.add("loaded");
+  setTimeout(() => {
+    document.querySelector("#preloader").style.display = "none";
+  }, 3000);
+});
