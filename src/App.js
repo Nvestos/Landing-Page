e3e65a7b9e565d4aeb2c1dae38dd3c34c209c9c1
Ogurinka Tammy Benjamin <ogurinkabenjamin@gmail.com>
2023-05-09 16:37:26 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ const app = createApp({
       msg: "Hi, Nvestos!",
       isDarkMode: false,
       showOptIn: false,
+      isLoading: false,
+      successState: {
+        show: false,
+        message: "",
+      },
       currentYear: new Date().getFullYear(),
+      user: {
+        email: "",
+        name: "",
+      },
     };
   },
   mounted() {
@@ -19,6 +28,55 @@ const app = createApp({
     },
   },
   methods: {
+    async joinWaitList() {
+      if (
+        !this.user.email ||
+        !this.user.name ||
+        !this.user.email.trim() ||
+        !this.user.name.trim()
+      ) {
+        toastr.error("Email and name are required.");
+        return;
+      }
+      const emailRegex = /^\S+@\S+\.\S+$/;
+      if (!emailRegex.test(this.user.email)) {
+        toastr.error("Invalid email format");
+        return;
+      }
+      this.isLoading = true;
+      try {
+        await axios.post(
+          "https://api.nvestos.com/api/v1/auth/waitlist",
+          {
+            name: this.user.name,
+            email: this.user.email,
+          },
+          {
+            "Content-Type": "application/json",
+          }
+        );
+        this.successState = {
+          show: true,
+          message: `You have successfully joined the waiting list. Keep an eye on your email (${this.user.email}) for updates.`,
+        };
+      } catch (error) {
+        console.log(error);
+        toastr.error("An error occurred. Please try again");
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    formSubmitted() {
+      this.user = {
+        email: "",
+        name: "",
+      };
+      this.showOptIn = false;
+      this.successState = {
+        show: false,
+        message: "",
+      };
+    },
     toggleTheme() {
       const body = document.body;
       body.classList.toggle("dark");
--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+toastr.options = {
+  closeButton: true,
+  newestOnTop: false,
+  progressBar: true,
+  positionClass: "toast-top-center",
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: "300",
+  hideDuration: "1000",
+  timeOut: "5000",
+  extendedTimeOut: "1000",
+  showEasing: "swing",
+  hideEasing: "linear",
+  showMethod: "fadeIn",
+  hideMethod: "fadeOut",
+};
 window.addEventListener("load", function () {
   document.body.classList.add("loaded");
   setTimeout(() => {
